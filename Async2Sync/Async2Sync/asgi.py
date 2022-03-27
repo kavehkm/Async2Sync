dad449f624ab6a87c1ba2fc03c7e52e4f15ff604
kavehkm <mehrbaniankaveh@gmail.com>
2022-03-27 11:13:43 -0400

    finally update Project.asgi.py to include websocket protocol

--- Async2Sync/Async2Sync/asgi.py
+++ Async2Sync/Async2Sync/asgi.py
@@ -3,11 +3,20 @@ import os
 # dj
 from django.core.asgi import get_asgi_application
 # channels
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+# internal
+import chat.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Async2Sync.settings')
 
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
