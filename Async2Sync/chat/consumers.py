0d5e6c7019d1c2157f5286ba610d25d651accf12
kavehkm <mehrbaniankaveh@gmail.com>
2022-03-27 11:12:12 -0400

    create new consumers.py: 1) add basic ChatConsumer to handle room route basic operations: connect, disconnect, recieve and send

---
+++ Async2Sync/chat/consumers.py
@@ -0,0 +1,18 @@
+# standard
+import json
+# channels
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    """Chat Consumer"""
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        self.send(text_data=json.dumps({'message': message}))
